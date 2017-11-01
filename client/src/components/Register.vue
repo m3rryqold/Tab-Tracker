eb43c0a704e89f417ef8f5e82d5b1eb76ddb32e3
Merrygold <merrymcx@gmail.com>
2017-11-01 15:32:50 +0100

    Setting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h4>Register</h4>
+
+    <input type="text" name="email" v-model="email" placeholder="email"><br>
+    <input type="password" name="password" v-model="password" placeholder="password"><br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed to ', value)
+  //   }
+  // },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.email = 'hello world'
+  //   }, 2000)
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,13 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hola'
+    message: `Hi ${req.body.email}, you are registered ...`
   })
 })
 
-app.listen(process.env.PORT || 8081)
+var port = process.env.PORT || 8081
+app.listen(port, () => {
+  console.log('listening on port ' + port)
+})
